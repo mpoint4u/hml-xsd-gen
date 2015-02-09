35aef960718c8cdc7a2712d8d4482d60c3473aea
Marcus Pointner <marcus.pointner@gmail.com>
2015-02-09 22:23:46 +0200

    create the schemas with relative paths on Win7

--- run_hml-xsd-gen.bat
+++ run_hml-xsd-gen.bat
@@ -1,3 +1,3 @@
 neko run.n  ^
--xml C:\Users\marcus\Documents\GitHub\hml-xsd-gen\src\gen\codestructure.xml  ^
--xsd C:\Users\marcus\Documents\GitHub\hml-xsd-gen\src\gen\schemas
+-xml src\gen\codestructure.xml  ^
+-xsd src\gen\schemas
